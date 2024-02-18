c8677a37f9cf62b97ef54a7b9613f0fc84d73759
Liliana Del Carmen Chora Monar <116900601+lilichora@users.noreply.github.com>
2024-02-17 19:58:49 -0500

    Portafolio

---
+++ src/components/Resume/index.js
@@ -0,0 +1,100 @@
+import BarGraph from "./BarGraph";
+import DownloadIcon from "@mui/icons-material/Download";
+
+export default function Resume() {
+  return (
+    <div id="resume" className="min-h-screen flex-col text-center">
+      <div className="text-5xl font-bold py-8" data-aos={"slide-down"}>
+        Resumen
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-7 mt-7">
+        <ol class="relative border-l  border-gray-700 ml-9 mb-6">
+          <li class="mb-10" data-aos="zoom-in">
+            <span class="absolute flex items-center justify-center w-6 h-6   rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3  text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold  text-white ml-28">
+              Certificado del Inge SANG
+            </h3>
+            <time class="block pt-1 pb-3  text-sm font-normal leading-none  text-gray-500">
+              Finalizado on Febrero 17, 2024
+            </time>
+            <p class="mb-4 text-base font-normal  text-gray-400">
+             Aprendi sobre las tecnologia 4.0 y sus avances para un futuro con robots y maquinas inteligentes.
+            </p>
+            <a
+              href="https://www.udemy.com/certificate/UC-caa0fa20-ac15-4b08-96a9-2ba648e1b2d0/"
+              target="_blank"
+              rel="noreferrer"
+              class="inline-flex items-center px-4 py-2 mt-2 text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>{" "}
+              Link del Certificado
+            </a>
+          </li>
+          <li class="mb-10" data-aos="zoom-out" data-aos-delay="700">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+          </li>
+        </ol>
+
+        <div className="flex flex-col text-center" data-aos="slide-up">
+          <div className="text-3xl font-bold py-3">My Skills</div>
+          <div className="ml-16 flex flex-col gap-8 mt-6">
+            <BarGraph name={"JavaScript"} percent={20} />
+            <BarGraph name={"React"} percent={50} />
+            <BarGraph name={"TypeScript"} percent={10} />
+            <BarGraph name={"Java"} percent={10} />
+            <BarGraph name={"HTML/CSS"} percent={100} />
+            <BarGraph name={"SQL"} percent={85} />
+            <BarGraph name={"AWS"} percent={40} />
+          </div>
+
+          <a
+            href={require("../../assets/files/LilianaChora.pdf")}
+            download={"Dani's Resume"}
+            class="inline-flex items-center px-6 py-2 mt-12 mx-auto w-fit text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+          >
+            Download CV <DownloadIcon />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/constants/index.js
@@ -0,0 +1,95 @@
+export const projects = [
+  {
+    title: "Expense Tracker",
+    desc: "Aplicacion para el seguimiento de gastos. Hecho con ReactJs y Firebase. Esta aplicacion ayuda a las personas a llevar un registro de sus gastos y a mantener un control sobre ellos. Tambien se puede ver el historial de gastos y se puede eliminar los gastos que no son necesarios. ",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "https://w1.pngwing.com/pngs/304/367/png-transparent-apple-budget-finance-expense-app-store-debt-money-financial-management.png",
+    link: "https://gastapp-con-material-ui.vercel.app/",
+    code: "https://github.com/lilichora/gastapp-con-material-ui.git",
+  },
+  {
+    title: "Web-Chat-App",
+    desc: "An app made with NextJs to help each other communicate fastly and securely. We can also search for the friends with their username.Realtime updates on messages.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "ChakraUI",
+        color: "green-600",
+      },
+      {
+        name: "SocketIO",
+        color: "yellow-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "https://user-images.githubusercontent.com/88134306/227576773-06c3a1d2-0a1a-4ab2-8d1f-e06d71408410.png",
+    link: "https://web-chat-app-brown.vercel.app",
+    code: "https://github.com/daniel-jebarson/web-chat-app",
+  },
+  
+  {
+    title: "Stock Market",
+    desc: "Stock Market Website done with ReactJs as Frontend and Express as Backend. This websites gives practice for people those who want to start using stock market. It is based on realtime update of stock value.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "green-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109032447783469097/image.png",
+    link: "https://stock-market-seven.vercel.app/",
+    code: "https://github.com/daniel-jebarson/Stock-Market",
+  },
+  {
+    title: "You-Farm",
+    desc: "An app made with NextJs, Machine Learning and ExpressJs to help farmers to sell their crops without intermediate. It also suggests which crops are needed for the specific soil.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "MaterialUI",
+        color: "green-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "yellow-600",
+      },
+      {
+        name: "FireBase",
+        color: "blue-800",
+      },
+      {
+        name: "Machine Learning",
+        color: "orange-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109035277814611978/Screenshot_19.png",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+];
